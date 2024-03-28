e50814a04dac9c0b390c7e6b08c38062a82881d7
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-03-28 16:04:30 +0530

    minor handling changes

--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -2,41 +2,37 @@ import React, { useContext } from "react";
 import logo from "../icons/beckn-logo.svg";
 import { Context } from "../Store";
 
-export default function Header() {
+const Header = () => {
   const [state, setState] = useContext(Context);
 
   const handleLogout = () => {
-    // Implement your logout logic here
     setState({ userInfo: {} });
   };
 
   return (
     <div className="header">
       <img className="b-logo" src={logo} alt="React Logo" />
-      {/*<div className="user-details">
-        {state.userInfo.user && (
-          <div className="b-padding">
-            Name: <b>{state.userInfo.user}</b>
-          </div>
-        )}
-        {state.userInfo.name_org && (
-          <div className="b-padding">
-            Organisation Name: <b>{state.userInfo.name_org}</b>
-          </div>
-        )}
-        {state.userInfo.name_role_timestamp && (
-          <div>
-            Role in the network: <b>{state.userInfo.name_role_timestamp}</b>
-          </div>
-        )}
-        {state.userInfo.user && <button onClick={handleLogout}>Logout</button>}
-        </div>*/}
-      <button className="ml-auto mx-2 rounded">
-        <a href="/login">Login</a>
-      </button>
-      <button className="rounded">
-        <a href="/signup">SignUp</a>
-      </button>
+      {state.userInfo.googleId ? (
+        <img
+          className="profile-icon"
+          src={state.userInfo.image}
+          alt="Profile Icon"
+        />
+      ) : (
+        <div>
+          <button className="ml-auto mx-2 rounded">
+            <a href="/login">Login</a>
+          </button>
+          <button className="rounded">
+            <a href="/signup">SignUp</a>
+          </button>
+        </div>
+      )}
+      {state.userInfo.googleId && (
+        <button onClick={handleLogout}>Logout</button>
+      )}
     </div>
   );
-}
+};
+
+export default Header;
--- client/src/component/SignUp.js
+++ client/src/component/SignUp.js
@@ -3,36 +3,29 @@ import axios from "axios";
 import { GoogleLogin } from "react-google-login";
 
 const SignUp = () => {
-  // State for storing form data
   const [formData, setFormData] = useState({});
 
-  // Function to handle successful Google login
   const handleGoogleLoginSuccess = async (googleUser) => {
-    // Extract user profile information from Google user object
     const profile = googleUser.getBasicProfile();
-    // Prepare data to send to the backend
     const googleData = {
       email: profile.getEmail(),
-      username: profile.getEmail(), // Using email as username
-      image: profile.getImageUrl(), // User's profile image URL
-      googleId: profile.getId(), // Google ID
+      username: profile.getEmail(),
+      image: profile.getImageUrl(),
+      googleId: profile.getId(),
     };
 
     try {
-      // Send a POST request to the backend API to sign up the user
       const response = await axios.post("/api/signup", googleData);
-      console.log(response.data); // Log the response data
+      console.log(response.data);
     } catch (error) {
-      console.error("Error submitting Google signup form:", error); // Log any errors
+      console.error("Error submitting Google signup form:", error);
     }
   };
 
-  // Function to handle Google login failure
   const handleGoogleLoginFailure = (error) => {
-    console.error("Google login failed:", error); // Log the error message
+    console.error("Google login failed:", error);
   };
 
-  // Log Google Client ID retrieved from environment variables
   console.log(
     "Google Client ID from backend:",
     process.env.REACT_APP_GOOGLE_ID
@@ -41,14 +34,12 @@ const SignUp = () => {
   return (
     <div className="container my-5">
       <h2>Sign Up</h2>
-      {/* Google Login component */}
       <GoogleLogin
-        clientId={process.env.REACT_APP_GOOGLE_ID} // Google Client ID
-        onSuccess={handleGoogleLoginSuccess} // Success callback function
-        onFailure={handleGoogleLoginFailure} // Failure callback function
-        cookiePolicy={"single_host_origin"} // Cookie policy for login
+        clientId={process.env.REACT_APP_GOOGLE_ID}
+        onSuccess={handleGoogleLoginSuccess}
+        onFailure={handleGoogleLoginFailure}
+        cookiePolicy={"single_host_origin"}
         render={(renderProps) => (
-          // Button to trigger Google login
           <button
             className="btn btn-primary"
             onClick={renderProps.onClick}
